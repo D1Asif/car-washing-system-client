f007bc29103ec138017706f3cc6d3dcb405befe7
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-10-15 00:22:27 +0600

    Sevice management table populated with data

--- src/components/dashboard/admin/ServiceManagement.tsx
+++ src/components/dashboard/admin/ServiceManagement.tsx
@@ -1,15 +1,80 @@
+import { Dropdown, DropdownAction, DropdownContent, DropdownItem, DropdownList, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "keep-react";
 import AddNewServiceModal from "./AddNewServiceModal";
+import { DotsThreeOutlineVertical } from "phosphor-react";
+import { useGetAllServicesQuery } from "../../../redux/features/service/serviceApi";
 
+type TService = {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  duration: number;
+  tags: string[];
+  isDeleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
 
 export default function ServiceManagement() {
+  const tableHeading = ['Service Name', 'Price', 'Duration', 'Tags'];
+
+  const { data } = useGetAllServicesQuery("");
+
+  const services = data?.data;
+
   return (
     <div>
-      <div className="flex justify-between items-center mt-2">
-        <h1 className="text-heading-6 font-semibold">
-          Service Management
-        </h1>
-        <AddNewServiceModal />
+      <div className="px-0 my-4">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-5">
+            <h2 className="text-heading-6 font-semibold text-metal-900 dark:text-white">Service Management</h2>
+          </div>
+          <div className="flex items-center gap-5">
+            <AddNewServiceModal />
+          </div>
+        </div>
       </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            {
+              tableHeading.map((heading) => (
+                <TableHead key={heading}>
+                  <div className="w-[200px]">{heading}</div>
+                </TableHead>
+              ))
+            }
+            <TableHead></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {services?.map((service: TService) => (
+            <TableRow>
+              <TableCell>{service.name}</TableCell>
+              <TableCell>{service.price}</TableCell>
+              <TableCell>{service.duration}</TableCell>
+              <TableCell>{service?.tags?.join(", ") || "None"}</TableCell>
+              {/* <TableCell>34</TableCell> */}
+              <TableCell>
+                <Dropdown>
+                  <DropdownAction asChild>
+                    <button>
+                      <DotsThreeOutlineVertical className="size-4 fill-metal-900 dark:fill-white" />
+                    </button>
+                  </DropdownAction>
+                  <DropdownContent className="max-w-[200px] border border-metal-100 p-3">
+                    <DropdownList>
+                      <DropdownItem>Edit</DropdownItem>
+                      <DropdownItem>Delete</DropdownItem>
+                    </DropdownList>
+                  </DropdownContent>
+                </Dropdown>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   )
 }
